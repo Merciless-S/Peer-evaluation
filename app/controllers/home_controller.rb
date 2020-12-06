59e455c8921e88a83f500066b266c763148592d0
MarcGuiselin <marc@guiselin.com>
2020-12-05 21:43:51 -0500

    Redirect logged-in admins to teams page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
-  def index
-  end
+  before_action :authorize
 
-  def show
+  def index
+    # Admins don't see anything on home page, so take them to teams page
+    redirect_to '/teams' if logged_in? and current_user.is_admin?
   end
 end
